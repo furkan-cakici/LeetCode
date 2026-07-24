e17c2f084b4a7bfdcf1e20583eb06100fa4d498a
Furkan ÇAKICI <147829415+furkan-cakici@users.noreply.github.com>
2026-07-24 08:36:48 +0300

    Update Median_of_Two_Sorted_Arrays.py

--- Median_of_Two_Sorted_Arrays.py
+++ Median_of_Two_Sorted_Arrays.py
@@ -1,1 +1,29 @@
+class Solution:
+    def findMedianSortedArrays(self, nums1, nums2):
 
+        if len(nums1) > len(nums2):
+            nums1, nums2 = nums2, nums1
+            
+        m, n = len(nums1), len(nums2)
+        low, high = 0, m
+        
+        while low <= high:
+            partitionX = (low + high) // 2
+            partitionY = (m + n + 1) // 2 - partitionX
+            
+            maxX = float('-inf') if partitionX == 0 else nums1[partitionX - 1]
+            minX = float('inf') if partitionX == m else nums1[partitionX]
+            
+            maxY = float('-inf') if partitionY == 0 else nums2[partitionY - 1]
+            minY = float('inf') if partitionY == n else nums2[partitionY]
+            
+            if maxX <= minY and maxY <= minX:
+                if (m + n) % 2 == 1:
+                    return float(max(maxX, maxY))
+                else:
+                    return (max(maxX, maxY) + min(minX, minY)) / 2.0
+                    
+            elif maxX > minY:
+                high = partitionX - 1
+            else:
+                low = partitionX + 1
