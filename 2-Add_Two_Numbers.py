da215214f9f165624e21d9458a86792d16366e28
Furkan ÇAKICI <147829415+furkan-cakici@users.noreply.github.com>
2026-07-27 08:25:06 +0300

    Refactor addTwoNumbers method and remove tests

--- 2-Add_Two_Numbers.py
+++ 2-Add_Two_Numbers.py
@@ -1,12 +1,5 @@
-from typing import Optional
-
-class ListNode:
-    def __init__(self, val=0, next=None):
-        self.val = val
-        self.next = next
-
-class Solution:
-    def addTwoNumbers(self, l1: Optional[ListNode], l2: Optional[ListNode]) -> Optional[ListNode]:
+class Solution(object):
+    def addTwoNumbers(self, l1, l2):
         dummy_head = ListNode(0)
         current = dummy_head
         carry = 0
@@ -26,36 +19,3 @@ class Solution:
                 l2 = l2.next
                 
         return dummy_head.next
-
-def list_to_linkedlist(elements: list) -> Optional[ListNode]:
-    dummy = ListNode()
-    current = dummy
-    for el in elements:
-        current.next = ListNode(el)
-        current = current.next
-    return dummy.next
-
-def linkedlist_to_list(node: Optional[ListNode]) -> list:
-    result = []
-    while node:
-        result.append(node.val)
-        node = node.next
-    return result
-
-if __name__ == "__main__":
-    solution = Solution()
-
-    l1_ex1 = list_to_linkedlist([2, 4, 3])
-    l2_ex1 = list_to_linkedlist([5, 6, 4])
-    result_node_1 = solution.addTwoNumbers(l1_ex1, l2_ex1)
-    print(f"Örnek 1 Sonucu: {linkedlist_to_list(result_node_1)} | Beklenen: [7, 0, 8]")
-
-    l1_ex2 = list_to_linkedlist([0])
-    l2_ex2 = list_to_linkedlist([0])
-    result_node_2 = solution.addTwoNumbers(l1_ex2, l2_ex2)
-    print(f"Örnek 2 Sonucu: {linkedlist_to_list(result_node_2)} | Beklenen: [0]")
-
-    l1_ex3 = list_to_linkedlist([9, 9, 9, 9, 9, 9, 9])
-    l2_ex3 = list_to_linkedlist([9, 9, 9, 9])
-    result_node_3 = solution.addTwoNumbers(l1_ex3, l2_ex3)
-    print(f"Örnek 3 Sonucu: {linkedlist_to_list(result_node_3)} | Beklenen: [8, 9, 9, 9, 0, 0, 0, 1]")
