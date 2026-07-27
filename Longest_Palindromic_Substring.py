dd8452927c70c2c88b107562d8197a9967d39c3d
Furkan ÇAKICI <147829415+furkan-cakici@users.noreply.github.com>
2026-07-27 08:17:25 +0300

    Update Longest_Palindromic_Substring.py

--- Longest_Palindromic_Substring.py
+++ Longest_Palindromic_Substring.py
@@ -1,1 +1,25 @@
-
+class Solution:
+    def longestPalindrome(self, s):
+        if not s or len(s) < 2:
+            return s
+            
+        start = 0
+        max_len = 0
+        
+        def expand_around_center(left, right):
+            while left >= 0 and right < len(s) and s[left] == s[right]:
+                left -= 1
+                right += 1
+            return right - left - 1
+            
+        for i in range(len(s)):
+            len1 = expand_around_center(i, i)
+            len2 = expand_around_center(i, i + 1)
+            
+            curr_max = max(len1, len2)
+            
+            if curr_max > max_len:
+                max_len = curr_max
+                start = i - (curr_max - 1) // 2
+                
+        return s[start:start + max_len]
