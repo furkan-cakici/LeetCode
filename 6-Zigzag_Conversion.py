f982d2d8a423bd1816e1a4ee10cc27b7c80fae8f
Furkan ÇAKICI <147829415+furkan-cakici@users.noreply.github.com>
2026-07-28 13:16:51 +0300

    Create 6-Zigzag_Conversion.py

---
+++ 6-Zigzag_Conversion.py
@@ -0,0 +1,21 @@
+class Solution(object):
+    def convert(self, s, numRows):
+        if numRows == 1 or numRows >= len(s):
+            return s
+        
+        rows = [""] * numRows
+        curr_row = 0
+        going_down = False
+        
+        for char in s:
+            rows[curr_row] += char
+            
+            if curr_row == 0 or curr_row == numRows - 1:
+                going_down = not going_down
+            
+            if going_down:
+                curr_row += 1
+            else:
+                curr_row -= 1
+                
+        return "".join(rows)
